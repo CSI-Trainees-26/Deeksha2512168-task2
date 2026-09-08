20cbccb7d0083a6dcf025350123dccb1e961300f
DEEKSHA-123456 <deekshashivhare831@gmail.com>
2026-09-08 23:45:20 +0530

    new

---
+++ timer.js
@@ -0,0 +1,5 @@
+const timerDisplay = document.getElementById("timer-display");
+let minutes = 0o0;
+let seconds = 0;
+timerDisplay.textContent = minutes + ":" + seconds;
+timerDisplay.textContent = minutes + ":" + formattedSeconds;
