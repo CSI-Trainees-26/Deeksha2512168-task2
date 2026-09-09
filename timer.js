10fa22f2a2638baf119c03c74ef4018715ea152f
DEEKSHA-123456 <deekshashivhare831@gmail.com>
2026-09-09 12:35:42 +0530

    timer

--- timer.js
+++ timer.js
@@ -1,5 +1,51 @@
 const timerDisplay = document.getElementById("timer-display");
-let minutes = 0o0;
+const startBtn = document.getElementById("startBtn")
+const pauseBtn = document.getElementById("pauseBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+let minutes = 0;
 let seconds = 0;
-timerDisplay.textContent = minutes + ":" + seconds;
-timerDisplay.textContent = minutes + ":" + formattedSeconds;
+let timer;
+
+let formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+let formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+timerDisplay.textContent = formattedMinutes + ":" + formattedSeconds;
+
+
+startBtn.addEventListener("click",function(){
+    if(timer){
+        return;
+    }
+    timer = setInterval(function() {
+     seconds++;
+
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    let formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    let formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+    timerDisplay.textContent = formattedMinutes+ ":" + formattedSeconds;
+}, 1000);
+sss
+    });
+
+    pauseBtn.addEventListener("click",function(){
+        clearInterval(timer);
+        timer=null;
+    });
+
+    resetBtn.addEventListener("click", function() {
+    clearInterval(timer);
+
+    timer = null;
+    minutes = 0;
+    seconds = 0;
+
+    timerDisplay.textContent = "00:00";
+});
+
+
